60f273fd05cd026ca2d78d1ff845ecc75135eaa8
jon-jacky <jon@u.washington.edu>
2011-06-26 21:03:47 -0700

    rename witch/ to grail/, make flip/ logic/ and grail/ into packages, flip_path only adds path to FLIP_DIR that contains flip/

--- bin/flip_path.bat
+++ bin/flip_path.bat
@@ -1,3 +1,3 @@
-rem Possibly change FLIP_DIR
+rem change FLIP_DIR to suit
 set FLIP_DIR=%USER%\FLiP-1.1
-set PYTHONPATH=%FLIP_DIR%\flip\logic;%FLIP_DIR%\flip\test;%FLIP_DIR%\flip\witch;%PYTHONPATH%
+set PYTHONPATH=%FLIP_DIR%;%PYTHONPATH%
